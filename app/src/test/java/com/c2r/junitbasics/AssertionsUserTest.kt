a0265000ae0b90f6ee148783820f99564575a37e
Robinnsson Cifuentes Ramírez <robinnsson@gmail.com>
2022-04-17 20:45:20 -0500

    @BeforeClass, @AfterClass y @JvmStatic

--- app/src/test/java/com/c2r/junitbasics/AssertionsUserTest.kt
+++ app/src/test/java/com/c2r/junitbasics/AssertionsUserTest.kt
@@ -1,34 +1,41 @@
 package com.c2r.junitbasics
 
-import org.junit.After
+import org.junit.*
 import org.junit.Assert.*
-import org.junit.Before
-
-import org.junit.Test
 
 class AssertionsUserTest {
 
     /**
-     * Se emplea las anotaciones @Before cuando las variables se emplean en varias pruebas y estas se realizan en la misma clase
-     * La anotacion @After se utiliza para limpiar dichas variables.
-     * Esto se emplea solo cuando no es costoso cargar nuevamente las variables.
+     * Se emplea las anotaciones @BeforeClass cuando las variables se emplean en varias pruebas y estas se realizan en la misma clase
+     * La anotacion @AfterClass se utiliza para limpiar dichas variables.
+     * Esto se emplea cuando es costoso cargar nuevamente las variables.
+     * Se deben acompañar de la anotacion @JvmStatic
      */
 
     private lateinit var bot: User
-    private lateinit var juan: User
 
-    // Esto se ejecuta antes de cada prueba, carga inicialmente todas las variables
+    companion object{
+        private lateinit var juan: User
+
+        @BeforeClass @JvmStatic
+        fun setupCommon() {
+            juan = User("Juan", 18, true)
+        }
+        
+        @AfterClass @JvmStatic
+        fun tearDownCommon() {
+            juan = User()
+        }
+    }
+
     @Before
     fun setup() {
         bot = User("8bit", 1, false)
-        juan = User("Juan", 18, true)
     }
 
-    // Esto se encarga de limpiar las variables
     @After
     fun tearDown() {
         bot = User()
-        juan = User()
     }
 
     @Test
@@ -41,6 +48,18 @@ class AssertionsUserTest {
     @Test
     fun checkNotNullUserTest() {
         assertNotNull(juan)
+    }
+
+
+    @Test
+    fun checkNotSameUsersTest(){
+        assertNotSame(bot, juan)
+    }
+
+    @Test
+    fun checkSameUsersTest(){
+        val copyJuan = juan
+        assertSame(copyJuan, juan)
     }
 
 }
